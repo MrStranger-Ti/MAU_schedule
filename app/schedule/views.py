cdbeea363ae304680410b434a2b346e93f30ca61
Андрей Сорокожердьев <sorokozherdyev@gmail.com>
2024-03-14 19:02:32 +0300

    реализована пагинация расписания по неделям

--- app/schedule/views.py
+++ app/schedule/views.py
@@ -1,14 +1,21 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
-
-from schedule.tasks import get_schedule_data
 
 
 class IndexPageView(LoginRequiredMixin, TemplateView):
     template_name = 'schedule/index.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['schedule'] = self.request.user.get_schedule()
-        return context
+    def get(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        context = self.get_context_data(*args, **kwargs)
+
+        schedule = self.request.user.get_schedule()
+        paginator = Paginator(list(schedule.items()), 6)
+
+        page = request.GET.get('page', 1)
+        page_obj = paginator.get_page(page)
+        context['page_obj'] = page_obj
+
+        return render(request, self.template_name, context=context)
