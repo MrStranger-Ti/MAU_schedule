7ed5ac085c8f9e754b2e1afd7b8235633842dbd9
Андрей Сорокожердьев <sorokozherdyev@gmail.com>
2024-05-01 02:20:17 +0300

    celery развернут в docker compose

--- app/app/celery.py
+++ app/app/celery.py
@@ -6,13 +6,13 @@ from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
-app = Celery('mau_schedule')
+app = Celery('app')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
     'check_and_delete_note': {
         'task': 'notes.tasks.delete_expired_notes',
-        'schedule': 30,
+        'schedule': crontab(minute=0, hour=0),
     },
 }
--- app/notes/tasks.py
+++ app/notes/tasks.py
@@ -6,7 +6,6 @@ from notes.models import Note
 
 @app.task
 def delete_expired_notes():
-    print('fdsffsdfsf')
     curr_date = date.today()
-    expired_notes = Note.objects.filter(expired_da=curr_date)
+    expired_notes = Note.objects.filter(expired_date__lte=curr_date)
     expired_notes.delete()
