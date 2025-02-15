34c04dfb3dd223d8c1bfef4402a66a16f2b64e6b
Andrey Sorokozherdev <asorokozherdyev@gmail.com>
2025-02-15 21:28:45 +0300

    В процессе: написание документации pen

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,6 @@ ENV PYTHONUNBUFFERED=1
 
 WORKDIR usr/src/app
 
-
 RUN pip install --upgrade pip poetry
 RUN poetry config virtualenvs.create false --local
 COPY pyproject.toml poetry.lock ./
--- app/schedule/api/views.py
+++ app/schedule/api/views.py
@@ -1,5 +1,12 @@
 from django.conf import settings
-from drf_spectacular.utils import extend_schema, OpenApiResponse, OpenApiExample
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import (
+    extend_schema,
+    OpenApiResponse,
+    OpenApiExample,
+    OpenApiRequest,
+    OpenApiParameter,
+)
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -14,8 +21,6 @@ from schedule.parser import (
 )
 from utils.local import get_json
 
-a = get_json(settings.BASE_DIR / "schedule/api/swagger_examples/group_schedule.json")
-
 
 class GroupScheduleApiView(APIView, ParserResponseViewMixin):
     permission_classes = [IsAuthenticated]
@@ -26,14 +31,19 @@ class GroupScheduleApiView(APIView, ParserResponseViewMixin):
         description="User must have institute, course and group.",
         responses={
             200: OpenApiResponse(
-                response="",
+                response=OpenApiTypes.OBJECT,
                 examples=[
                     OpenApiExample(
                         "Group schedule example",
-                        value=a,
+                        value=get_json(
+                            settings.BASE_DIR
+                            / "schedule/api/swagger_examples/group_schedule.json",
+                        ),
                     )
                 ],
-            )
+            ),
+            400: OpenApiResponse(description="Invalid institute, course or group"),
+            503: OpenApiResponse(description="Official schedule is unavailable now"),
         },
     )
     def get(self, request: Request) -> Response:
@@ -50,6 +60,34 @@ class GroupScheduleApiView(APIView, ParserResponseViewMixin):
 class TeachersKeysApiView(APIView, ParserResponseViewMixin):
     permission_classes = [IsAuthenticated]
 
+    @extend_schema(
+        tags=["Schedule"],
+        summary="Getting teachers keys for teacher schedule",
+        description="Query param 'name' is required",
+        parameters=[
+            OpenApiParameter(
+                name="name",
+                type=OpenApiTypes.STR,
+                location=OpenApiParameter.QUERY,
+                required=True,
+            )
+        ],
+        responses={
+            200: OpenApiResponse(
+                response=OpenApiTypes.OBJECT,
+                examples=[
+                    OpenApiExample(
+                        "Teachers keys example",
+                        value=get_json(
+                            settings.BASE_DIR
+                            / "schedule/api/swagger_examples/teachers_keys.json",
+                        ),
+                    )
+                ],
+            ),
+            503: OpenApiResponse(description="Official schedule is unavailable now"),
+        },
+    )
     def get(self, request: Request) -> Response:
         name = request.query_params.get("name", "")
         parser_response = get_teachers_keys(name=name)
@@ -59,6 +97,35 @@ class TeachersKeysApiView(APIView, ParserResponseViewMixin):
 class TeacherScheduleApiView(APIView, ParserResponseViewMixin):
     permission_classes = [IsAuthenticated]
 
+    @extend_schema(
+        tags=["Schedule"],
+        summary="Getting teacher schedule",
+        description="Need to get teacher key",
+        parameters=[
+            OpenApiParameter(
+                name="teacher_key",
+                type=OpenApiTypes.STR,
+                location=OpenApiParameter.PATH,
+                required=True,
+            )
+        ],
+        responses={
+            200: OpenApiResponse(
+                response=OpenApiTypes.OBJECT,
+                examples=[
+                    OpenApiExample(
+                        "Teacher schedule example",
+                        value=get_json(
+                            settings.BASE_DIR
+                            / "schedule/api/swagger_examples/teacher_schedule.json",
+                        ),
+                    )
+                ],
+            ),
+            400: OpenApiResponse(description="Invalid teacher key"),
+            503: OpenApiResponse(description="Official schedule is unavailable now"),
+        },
+    )
     def get(self, request: Request, teacher_key: str) -> Response:
         period = request.query_params.get("period")
         parser_response = get_teacher_schedule(teacher_key=teacher_key, period=period)
@@ -68,6 +135,25 @@ class TeacherScheduleApiView(APIView, ParserResponseViewMixin):
 class SchedulePeriodsApiView(APIView, ParserResponseViewMixin):
     permission_classes = [IsAuthenticated]
 
+    @extend_schema(
+        tags=["Schedule"],
+        summary="Getting schedule periods",
+        responses={
+            200: OpenApiResponse(
+                response=OpenApiTypes.OBJECT,
+                examples=[
+                    OpenApiExample(
+                        "Teacher schedule example",
+                        value=get_json(
+                            settings.BASE_DIR
+                            / "schedule/api/swagger_examples/teachers_keys.json",
+                        ),
+                    )
+                ],
+            ),
+            503: OpenApiResponse(description="Official schedule is unavailable now"),
+        },
+    )
     def get(self, request: Request) -> Response:
         parser_response = get_periods()
         return self.get_response(parser_response)
